897059b0839e8cb3e76235949f2eabf6125d35d7
Usairim Isani <usairim.isani@outlook.com>
2020-08-25 21:30:44 +0500

    Flip, Upsample, Downsample.

--- lab4.m
+++ lab4.m
@@ -3,14 +3,16 @@ clear all;
 clc;
 
 % Figure One with x(n)
+
 x = [ 0 0 0 0 1 1 1 1];
 n = -4:3 ;
 stem(n,x);
-title("DSP");
+title("x(n)");
 xlabel("n");
 ylabel("Amplitude");
 
 % x(n-1)
+
 figure
 stem(n+1,x);
 title("x(n-1) Delayed");
@@ -24,3 +26,31 @@ stem(n-1,x);
 title("x(n+1) Advanced");
 xlabel("n+1");
 ylabel("Ampliture");
+
+% Flip 
+
+y = fliplr(x);
+figure
+stem(n,y);
+title("x(-n)");
+xlabel("n");
+ylabel("Amplitude");
+
+% Downsample 
+y1 = downsample(x,2);
+n1 = -2:1:1;
+figure
+stem(n1,y1);
+title("x(-n)");
+xlabel("n");
+ylabel("Amplitude");
+
+% Upsample
+
+y2 = upsample(x,2);
+n2 = -8:1:7;
+figure
+stem(n2,y2);
+title("x(-n)");
+xlabel("n");
+ylabel("Amplitude");
